a900d53d0dcc3033843ba5c3cebd3d7cada614e8
Victoria Helin <vickanhelin@hotmail.com>
2026-02-11 22:00:49 +0100

    lagt till js och css för att dölja och visa länkar vid klick hamburgermeny

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const hamburger = document.getElementById("hamburger");
+
+    hamburger.addEventListener("click", ()=>{
+        
+        const nav = document.getElementById("headerNav");
+
+         if(nav.className ==="topnav") {
+                    nav.className += " responsive";
+                } else {
+                    nav.className = "topnav";
+                }
+                console.log(nav.className);
+    });
+})
