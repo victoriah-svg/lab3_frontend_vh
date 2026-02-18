a645f2acfd556868a3172579518b8c2a9d215e8d
Victoria Helin <vickanhelin@hotmail.com>
2026-02-18 13:15:44 +0100

    lagt till keyframe för scale på heading

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,56 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
     /*Hamburgermeny*/
     const hamburger = document.getElementById("hamburger");
 
-    hamburger.addEventListener("click", ()=>{
-        
+    hamburger.addEventListener("click", () => {
+
         const nav = document.getElementById("headerNav");
 
-         if(nav.className ==="topnav") {
-                    nav.className += " responsive";
-                } else {
-                    nav.className = "topnav";
-                }
-                console.log(nav.className);
+        if (nav.className === "topnav") {
+            nav.className += " responsive";
+        } else {
+            nav.className = "topnav";
+        }
+        console.log(nav.className);
     });
 
     /* Stylingknapp */
     const styleBtn = document.getElementById("styleBtn");
     styleBtn.addEventListener("click", stylePage);
 
+   // styleBtn.addEventListener("click", wiggle);
 })
 
 /* styling av sidan när knapp klickas */
-function stylePage () {
+function stylePage() {
     const headingEl = document.getElementById("animationHeading");
 
-     if (headingEl.style.animationPlayState == "running" &
+    if (headingEl.style.animationPlayState == "running" &
         styleBtn.style.animationPlayState == "running"
-     ) {
-            headingEl.style.animationPlayState = "paused";
-            styleBtn.style.animationPlayState = "paused";
-             console.log("from running to paused");
-        } else {
-            headingEl.style.animationPlayState = "running"; 
-            styleBtn.style.animationPlayState = "running"; 
-            console.log("from paused to running")
-        }
+    ) {
+        headingEl.style.animationPlayState = "paused";
+        styleBtn.style.animationPlayState = "paused";
+
+    } else {
+        headingEl.style.animationPlayState = "running";
+        styleBtn.style.animationPlayState = "running";
+
+    }
 }
+
+/*function wiggle() {
+    
+   const headingEl = document.getElementById("animationHeading");
+    
+        if (
+            smallPic1.style.animationPlayState == "running"
+        ) {
+            smallPic1.style.animationPlayState = "paused";
+        } else {
+            smallPic1.style.animationPlayState = "running";
+        }
+    
+}*/
