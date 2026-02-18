c6d990e7bfd02c0d1b5bacde4d93646c87f62e8c
Victoria Helin <vickanhelin@hotmail.com>
2026-02-18 11:36:44 +0100

    lagt till animation för styling heading och knapp samt hovereffekt länkar

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", ()=>{
+
+    /*Hamburgermeny*/
     const hamburger = document.getElementById("hamburger");
 
     hamburger.addEventListener("click", ()=>{
@@ -15,5 +17,25 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 console.log(nav.className);
     });
 
-    
+    /* Stylingknapp */
+    const styleBtn = document.getElementById("styleBtn");
+    styleBtn.addEventListener("click", stylePage);
+
 })
+
+/* styling av sidan när knapp klickas */
+function stylePage () {
+    const headingEl = document.getElementById("animationHeading");
+
+     if (headingEl.style.animationPlayState == "running" &
+        styleBtn.style.animationPlayState == "running"
+     ) {
+            headingEl.style.animationPlayState = "paused";
+            styleBtn.style.animationPlayState = "paused";
+             console.log("from running to paused");
+        } else {
+            headingEl.style.animationPlayState = "running"; 
+            styleBtn.style.animationPlayState = "running"; 
+            console.log("from paused to running")
+        }
+}
